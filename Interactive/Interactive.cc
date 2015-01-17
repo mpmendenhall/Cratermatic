44d5b63400cfb7e4e0ad0120576fbda7fce5d40c
Michael Mendenhall <mpm@caltech.edu>
2015-01-17 14:20:37 -0500

    Update license notice to GPL v3 from GPL v2

--- Interactive/Interactive.cc
+++ Interactive/Interactive.cc
@@ -5,7 +5,7 @@
 //
 // This program is free software; you can redistribute it and/or
 // modify it under the terms of the GNU General Public License
-// as published by the Free Software Foundation; either version 2
+// as published by the Free Software Foundation; either version 3
 // of the License, or (at your option) any later version.
 //
 // This program is distributed in the hope that it will be useful,
@@ -311,7 +311,7 @@ void Interactor::processCommand(){ //iterate through commandstream
 			printf("//\n");
 			printf("// This program is free software; you can redistribute it and/or\n");
 			printf("// modify it under the terms of the GNU General Public License\n");
-			printf("// as published by the Free Software Foundation; either version 2\n");
+			printf("// as published by the Free Software Foundation; either version 3\n");
 			printf("// of the License, or (at your option) any later version.\n");
 			printf("//\n");
 			printf("// This program is distributed in the hope that it will be useful,\n");
